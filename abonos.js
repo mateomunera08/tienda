5453ef3c0cce62b979e03fd2a0050c3905c1dac5
mateomunera <mateomunera2004@gmail.com>
2022-03-24 17:29:34 -0500

    abonos

---
+++ abonos.js
@@ -0,0 +1,43 @@
+let etiquetaBoton=document.getElementById("boton")
+etiquetaBoton.addEventListener("click",escuchar)
+function escuchar(event){
+    event.preventDefault()
+    console.log("hizo click")
+    let abonos=document.getElementById("abonos").value
+    console.log(abonos)
+    let nombres=document.getElementById("nombres").value
+    console.log(nombres)
+    let cedula=document.getElementById("cedula").value
+    console.log(cedula)
+    let correo=document.getElementById("correo").value
+    console.log(correo)
+    let cantidad=document.getElementById("cantidad").value
+    console.log(cantidad)
+
+    let valorSur=2000
+    let valorNorte=2500
+    let valorOriental=2500
+    let valorOcidental=3000
+    let valorVip=22000
+
+    if(abonos=="Sur=2000"){
+
+        alert("el total de su compra es "+(valorSur*cantidad))
+
+    }else if(abonos=="Norte=2500") {
+
+        alert("el total de su compra es "+(valorNorte*cantidad))
+
+    }else if(abonos=="Oriental=2500") {
+
+        alert("el total de su compra es "+(valorOriental*cantidad))
+    }else if(abonos=="Ocidental=3000") {
+
+        alert("el total de su compra es "+(valorOcidental*cantidad))
+    }else if (empanada=="Vip=22000") {
+
+        alert("el total de su compra es "+(valorVip*cantidad))
+    }
+
+
+}
--- uno.js
+++ uno.js
@@ -1,16 +1,44 @@
 let etiquetaBoton=document.getElementById("boton")
 etiquetaBoton.addEventListener("click",escuchar)
 function escuchar(event){
-event.preventDefault()
-console.log("hizo click")
-let empanada=document.getElementById("empanada").value
-console.log(empanada)
-let nombre=document.getElementById("nombre").value
-console.log(nombre)
-let cedula=document.getElementById("cedula").value
-console.log(cedula)
-let correo=document.getElementById("correo").value
-console.log(correo)
-let cantidad=document.getElementById("cantidad").value
-console.log(cantidad)
+    event.preventDefault()
+    console.log("hizo click")
+    let empanada=document.getElementById("empanada").value
+    console.log(empanada)
+    let nombre=document.getElementById("nombre").value
+    console.log(nombre)
+    let cedula=document.getElementById("cedula").value
+    console.log(cedula)
+    let correo=document.getElementById("correo").value
+    console.log(correo)
+    let cantidad=document.getElementById("cantidad").value
+    console.log(cantidad)
+
+    let valorCarne=2000
+    let valorPollo=2500
+    let valorMaicitos=2500
+    let valorFitnes=3000
+    let valorArgentina=2200
+
+    if(empanada=="Carne=2000"){
+
+        alert("el total de su compra es "+(valorCarne*cantidad))
+
+    }else if(empanada=="Pollo=2500") {
+
+        alert("el total de su compra es "+(valorPollo*cantidad))
+
+    }else if(empanada=="Maicitos=2500") {
+
+        alert("el total de su compra es "+(valorMaicitos*cantidad))
+    }else if(empanada=="Fitnes=3000") {
+
+        alert("el total de su compra es "+(valorFitnes*cantidad))
+    }else if (empanada=="Argentina=2200") {
+
+        alert("el total de su compra es "+(valorArgentina*cantidad))
+    }
+
+
 }
+
